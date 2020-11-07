6cc25fc553e07e16a6325320ee5df43da4fd2916
Entreprenerd <valeranijr@gmail.com>
2020-11-07 23:05:16 +0100

    13 Add Link to Product

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
 import products from '../products.json'
@@ -14,14 +15,18 @@ export default function Home() {
 
       {products.map(product => (
         <div key={product.name} className={styles.product}>
-          <div className={styles.product__Row}>
-            <div className={styles.product__ColImg}> 
-              <img src={fromImageToUrl(product.image)} />
-            </div>
-            <div className={styles.product__Col}>
-              {product.name} {product.price}
-            </div>
-          </div>
+          <Link href={`/products/${product.slug}`}>
+            <a>
+              <div className={styles.product__Row}>
+                <div className={styles.product__ColImg}> 
+                  <img src={fromImageToUrl(product.image)} />
+                </div>
+                <div className={styles.product__Col}>
+                  {product.name} {product.price}
+                </div>
+              </div>
+            </a>
+          </Link>
         </div>
       ))}
 
