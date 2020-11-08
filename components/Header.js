55ee48cf8ee6c8d8fd87b3fdcd2ea25c8b1518b0
Entreprenerd <valeranijr@gmail.com>
2020-11-08 13:19:18 +0100

    21 Go Back Functionality with Router

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,35 @@
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+
 import styles from '../styles/Header.module.css'
 
-export default () => (
-    <div>
-        
-        <div className={styles.title}>
-            <h1>
-                The E-Commerce
-            </h1>
-        </div>
+export default () => {
 
-    </div>
-)
+    const router = useRouter()
+    const isHome = router.pathname === "/"
+
+    const goBack = (event) => {
+        event.preventDefault()
+        router.back()
+    }
+
+    return (
+        <div className={styles.nav}>
+            {!isHome && 
+                <div className={styles.back}>
+                    <a href="#" onClick={goBack}>{"<"} Back </a>
+                </div>
+            }
+            <div className={styles.title}>
+                <Link href="/">
+                    <a>
+                        <h1>
+                            The E-Commerce
+                        </h1>
+                    </a>
+                </Link>
+            </div>
+
+        </div>
+    )
+}
