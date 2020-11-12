00adcca14d07014e4ab155acec1dcaa983981269
Entreprenerd <valeranijr@gmail.com>
2020-11-12 13:21:55 +0100

    37 Show Orders, add Loading Indicator

--- pages/account.js
+++ pages/account.js
@@ -7,11 +7,13 @@ import { API_URL } from '../utils/urls'
 
 const useOrders = (user, getToken) => {
     const [orders, setOrders] = useState([])
+    const [loading, setLoading] = useState(false)
 
     useEffect(() => {
         const fetchOrders = async () => {
             if(user) {
                 try {
+                    setLoading(true)
                     const token = await getToken()
                     const order_res = await fetch(`${API_URL}/orders`, {
                         headers: {
@@ -23,20 +25,21 @@ const useOrders = (user, getToken) => {
                 } catch (err) {
                     setOrders([])
                 }
+                setLoading(false)
             }
         }
 
         fetchOrders()
     }, [user])
 
-    return orders
+    return { orders, loading }
 }
 
 export default function Account() {
 
     const { user, logoutUser, getToken } = useContext(AuthContext)
 
-    const orders = useOrders(user, getToken)
+    const { orders, loading } = useOrders(user, getToken)
     console.log("Account.render orders", orders)
 
     if(!user){
@@ -56,6 +59,15 @@ export default function Account() {
             </Head>
 
             <h2>Account page</h2>
+
+            <h3>Your Orders</h3>
+            {loading && <p>Loading your orders</p>}
+            {orders.map(order => (
+                <div key={order.id}>
+                    {new Date(order.created_at).toLocaleDateString( 'en-EN' )} {order.product.name} ${order.total} {order.status}
+                </div>
+            ))}
+            <hr />
             <p>Logged in as: {user.email}</p>
             <a href="#" onClick={logoutUser}>Logout</a>
         </div>
